f307dbbb75f560e09ae2ea7ee264b48ff8b072f7
Admin <anhquan300503@gmail.com>
2023-12-06 12:25:26 +0700

    06/12/2023: đẩy source lên repository

---
+++ source/Project_63135188/Project_63135188/Models/TraCuuPhongThieuNguoi63135188.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_63135188.Models
+{
+  public class TraCuuPhongThieuNguoi63135188
+  {
+    public string maPhong { get; set; }
+    public int conThieu { get; set; }
+  }
+}
